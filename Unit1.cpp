08513db01600f6c9aadf6c3ed0f75b30adcb4540
LZR\admin <Lizhuoran1024@gmail.com>
2018-07-20 14:39:40 +0800

    commit 720 1439
    Scrollbar all custom

--- Unit1.cpp
+++ Unit1.cpp
@@ -12,10 +12,15 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
         : TForm(Owner)
 {
+
+    apploc = Application->ExeName;
+	int end = apploc.LastDelimiter("\\");
+    apploc = apploc.SubString(0, end);
 	song_count = 0;
     song_list_bottom_index = 0;
     current_song_index = 0;
@@ -26,22 +31,40 @@ __fastcall TForm1::TForm1(TComponent* Owner)
     mouse_key_down = false;
     mouse_x = 0;
     mouse_y = 0;
+
     scrollbar_min_value = 0;
     scrollbar_max_value = Image2->Width;
-	
 	scrollbar_min_pos = Image2->Left;
 	scrollbar_max_pos = Image2->Left + Image2->Width;
     scrollbar_length = scrollbar_max_pos - scrollbar_min_pos;
     scrollbar_enabled = false;
 
+    volu_trackbar_min_value = 0;
+    volu_trackbar_max_value = 10;
+    volu_trackbar_min_pos = Image3->Top - Image4->Height / 2;
+    volu_trackbar_max_pos = Image3->Top + Image3->Height - Image4->Height / 2;
+    volu_trackbar_length = volu_trackbar_max_pos - volu_trackbar_min_pos;
+
     Image1->Left = Image2->Left;
 	Image1->Parent->DoubleBuffered = true;
 
 	Label1->Caption = "No music playing now";
-    SpeedButton4->Glyph->LoadFromFile("D:\\KM\\icons\\delete_icon.bmp");
-    SpeedButton6->Glyph->LoadFromFile("D:\\KM\\icons\\seq_play_act_icon.bmp");
+    SpeedButton4->Glyph->LoadFromFile(apploc + "icons\\delete_icon.bmp");
+    SpeedButton6->Glyph->LoadFromFile(apploc + "icons\\seq_play_act_icon.bmp");
     updateTimeDisplay(0, Label2);
     updateTimeDisplay(0, Label4);
+
+    getSystemVolume();
+    Label6->Caption = AnsiString(current_volume);
+
+    lyric_displayer_array = new TLabel* [7];
+    lyric_displayer_array[0] = Label3;
+    lyric_displayer_array[1] = Label7;
+    lyric_displayer_array[2] = Label8;
+    lyric_displayer_array[3] = Label9;
+    lyric_displayer_array[4] = Label10;
+    lyric_displayer_array[5] = Label11;
+    lyric_displayer_array[6] = Label12;
 }
 //---------------------------------------------------------------------------
 /*
@@ -173,7 +196,7 @@ void __fastcall TForm1::loadMusicAndPlay(AnsiString fileloc)
 
          //Button2->Caption = "Pause";
 
-         SpeedButton1->Glyph->LoadFromFile("D:\\KM\\icons\\pause_icon.bmp");
+         SpeedButton1->Glyph->LoadFromFile(apploc + "\\icons\\pause_icon.bmp");
     }
 	
 }
@@ -385,7 +408,7 @@ void __fastcall TForm1::SpeedButton1Click(TObject *Sender)
     {
     	MediaPlayer1->Pause();
         is_playing = false;
-        SpeedButton1->Glyph->LoadFromFile("D:\\KM\\icons\\play_icon.bmp");
+        SpeedButton1->Glyph->LoadFromFile(apploc + "icons\\play_icon.bmp");
     }
     else
     {
@@ -393,7 +416,7 @@ void __fastcall TForm1::SpeedButton1Click(TObject *Sender)
         {
     		MediaPlayer1->Play();
         	is_playing = true;
-            SpeedButton1->Glyph->LoadFromFile("D:\\KM\\icons\\pause_icon.bmp");
+            SpeedButton1->Glyph->LoadFromFile(apploc + "icons\\pause_icon.bmp");
         }
     }
 }
@@ -441,12 +464,12 @@ void __fastcall TForm1::SpeedButton4Click(TObject *Sender)
     if(erase_activate)
     {
     	Label5->Caption = "Click item to delate";
-        SpeedButton4->Glyph->LoadFromFile("D:\\KM\\icons\\delete_act_icon.bmp");
+        SpeedButton4->Glyph->LoadFromFile(apploc + "icons\\delete_act_icon.bmp");
     }
     else
     {
     	Label5->Caption = "";
-        SpeedButton4->Glyph->LoadFromFile("D:\\KM\\icons\\delete_icon.bmp");
+        SpeedButton4->Glyph->LoadFromFile(apploc + "icons\\delete_icon.bmp");
     }
 }
 //---------------------------------------------------------------------------
@@ -490,27 +513,27 @@ void __fastcall TForm1::SpeedButton5Click(TObject *Sender)
 void __fastcall TForm1::SpeedButton6Click(TObject *Sender)
 {
 	mode = sequencial;
-    SpeedButton6->Glyph->LoadFromFile("D:\\KM\\icons\\seq_play_act_icon.bmp");
-    SpeedButton7->Glyph->LoadFromFile("D:\\KM\\icons\\single_play_icon.bmp");
-    SpeedButton8->Glyph->LoadFromFile("D:\\KM\\icons\\random_play_icon.bmp");
+    SpeedButton6->Glyph->LoadFromFile(apploc + "icons\\seq_play_act_icon.bmp");
+    SpeedButton7->Glyph->LoadFromFile(apploc + "icons\\single_play_icon.bmp");
+    SpeedButton8->Glyph->LoadFromFile(apploc + "icons\\random_play_icon.bmp");
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::SpeedButton7Click(TObject *Sender)
 {
  	mode = single;
-    SpeedButton6->Glyph->LoadFromFile("D:\\KM\\icons\\seq_play_icon.bmp");
-    SpeedButton7->Glyph->LoadFromFile("D:\\KM\\icons\\single_play_act_icon.bmp");
-    SpeedButton8->Glyph->LoadFromFile("D:\\KM\\icons\\random_play_icon.bmp");
+    SpeedButton6->Glyph->LoadFromFile(apploc + "icons\\seq_play_icon.bmp");
+    SpeedButton7->Glyph->LoadFromFile(apploc + "icons\\single_play_act_icon.bmp");
+    SpeedButton8->Glyph->LoadFromFile(apploc + "icons\\random_play_icon.bmp");
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TForm1::SpeedButton8Click(TObject *Sender)
 {
 	mode = random;
-    SpeedButton6->Glyph->LoadFromFile("D:\\KM\\icons\\seq_play_icon.bmp");
-    SpeedButton7->Glyph->LoadFromFile("D:\\KM\\icons\\single_play_icon.bmp");
-    SpeedButton8->Glyph->LoadFromFile("D:\\KM\\icons\\random_play_act_icon.bmp");
+    SpeedButton6->Glyph->LoadFromFile(apploc + "icons\\seq_play_icon.bmp");
+    SpeedButton7->Glyph->LoadFromFile(apploc + "icons\\single_play_icon.bmp");
+    SpeedButton8->Glyph->LoadFromFile(apploc + "icons\\random_play_act_icon.bmp");
 }
 //---------------------------------------------------------------------------
 
@@ -555,5 +578,106 @@ void __fastcall TForm1::Image1MouseUp(TObject *Sender, TMouseButton Button,
 //---------------------------------------------------------------------------
 
 
+void __fastcall TForm1::getSystemVolume()
+{
+	DWORD volume;
+    waveOutGetVolume(0, &volume);
+    //TrackBar1->Position = 0XFFFF - LOWORD(volume);
+    current_volume = LOWORD(volume) * (volu_trackbar_max_value / 65535.0);
+    setVolTrackbarPos(current_volume);
+    
+}
+/*
+void __fastcall TForm1::TrackBar1Change(TObject *Sender)
+{
+	int value = 0xffff - TrackBar1->Position;
+    DWORD volume;
+    waveOutSetVolume(0, TrackBar1->Position * (0xffff / 10.0));
+    current_volume = TrackBar1->Position;
+    Label6->Caption = AnsiString(current_volume);
+    if(current_volume == 0)
+    	SpeedButton9->Glyph->LoadFromFile(apploc + "icons\\mute_icon.bmp");
+    else
+        SpeedButton9->Glyph->LoadFromFile(apploc + "icons\\volu_icon.bmp");
+}
+*/
+//---------------------------------------------------------------------------
+
+
+
+void __fastcall TForm1::SpeedButton9Click(TObject *Sender)
+{
+	if(is_muted == false)
+    {
+    	is_muted = true;
+    	waveOutSetVolume(0, 0x0);
+        Label6->Caption = "0";
+    	SpeedButton9->Glyph->LoadFromFile(apploc + "icons\\mute_icon.bmp");
+        setVolTrackbarPos(0);
+    }
+    else
+    {
+    	waveOutSetVolume(0, current_volume * (0xffff / 10.0));
+        //TrackBar1->Position = current_volume;
+        setVolTrackbarPos(current_volume);
+        is_muted = false;
+        Label6->Caption = AnsiString(current_volume);
+    	SpeedButton9->Glyph->LoadFromFile(apploc + "icons\\volu_icon.bmp");
+    }
+}
+//---------------------------------------------------------------------------
+
+
+
+void __fastcall TForm1::Image4MouseDown(TObject *Sender,
+      TMouseButton Button, TShiftState Shift, int X, int Y)
+{
+	mouse_key_down = true;
+    mouse_x = X;
+    mouse_y = Y;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image4MouseUp(TObject *Sender, TMouseButton Button,
+      TShiftState Shift, int X, int Y)
+{
+	mouse_key_down = false;
+    mouse_x = X;
+    mouse_y = Y;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Image4MouseMove(TObject *Sender, TShiftState Shift,
+      int X, int Y)
+{
+	int desired_pos = Image4->Top - (mouse_y - Y);
+	if ((desired_pos >= volu_trackbar_min_pos) && (desired_pos < volu_trackbar_max_pos))
+	{
+		if(mouse_key_down == true)
+        {
+			Image4->Top = desired_pos;
+            int volume =  (volu_trackbar_max_pos - Image4->Top) * (volu_trackbar_max_value / static_cast<double>(volu_trackbar_length));
+            waveOutSetVolume(0, volume * (0xffff / static_cast<double>(volu_trackbar_max_value)));
+            current_volume = volume;
+            Label6->Caption = AnsiString(volume);
+            if(volume == 0)
+            	SpeedButton9->Glyph->LoadFromFile(apploc + "\\icons\\mute_icon.bmp");
+            else
+            	SpeedButton9->Glyph->LoadFromFile(apploc + "\\icons\\volu_icon.bmp");
+
+		}
+	}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::setVolTrackbarPos(int vol)
+{
+	if((vol >= 0)&&(vol <= volu_trackbar_max_value))
+    {
+    	int desired_pos = volu_trackbar_max_pos -
+        				vol * (volu_trackbar_length / static_cast<double>(volu_trackbar_max_value));
+        Image4->Top = desired_pos;
+    }
+}
 
 
